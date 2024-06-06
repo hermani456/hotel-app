a8059c865de28540b694368f99220a5aec734d64
hermani <hermani457@gmail.com>
2024-06-06 13:27:28 -0400

    refactor: Update roomItems in utils/index.js with correct IDs, titles, descriptions, and prices

--- src/utils/index.js
+++ src/utils/index.js
@@ -18,43 +18,34 @@ export const navItems = [
   },
   {
     id: 4,
-    name: "Sign In",
-    path: "/sign-in",
-  },
-  {
-    id: 5,
-    name: "Sign Up",
-    path: "/sign-up",
-  },
-  {
-    id: 6,
     name: "Dashboard",
     path: "/dashboard",
   },
 ];
-export const roomItems =[
+
+export const roomItems = [
   {
-    id:1,
-    title:'Pieza 1',
-    description: 'Pieza con vista al mar',
+    id: 1,
+    title: "Pieza 1",
+    description: "Pieza con vista al mar",
     price: "10.000",
   },
   {
-    id:2,
-    title:'Pieza 2',
-    description: 'Pieza con vista al cerro',
+    id: 2,
+    title: "Pieza 2",
+    description: "Pieza con vista al cerro",
     price: "20.000",
   },
   {
-    id:1,
-    title:'Pieza 3',
-    description: 'Habitacion con vista a la chucha',
+    id: 1,
+    title: "Pieza 3",
+    description: "Habitacion con vista a la chucha",
     price: "30.000",
   },
   {
-    id:1,
-    title:'Pieza 4',
-    description: 'Habitacion con vista a la cresta',
+    id: 1,
+    title: "Pieza 4",
+    description: "Habitacion con vista a la cresta",
     price: "40.000",
   },
 ];
